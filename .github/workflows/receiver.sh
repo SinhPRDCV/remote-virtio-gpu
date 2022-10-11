d852ebf947ae02b29d2c0c59fa8ba3263292d1c9
SinhPRDCV <92356461+SinhPRDCV@users.noreply.github.com>
2022-10-11 12:28:29 +0700

    Update receiver.sh
    Signed-off-by: SinhPRDCV <92356461+SinhPRDCV@users.noreply.github.com>

--- .github/workflows/receiver.sh
+++ .github/workflows/receiver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-~/actions-runner/_work/remote-virtio-gpu/remote-virtio-gpu/build/src/rvgpu-renderer/rvgpu-renderer -b 800x600@0,0 -p 55667 &
+/home/nghia/actions-runner/_work/remote-virtio-gpu/remote-virtio-gpu/build/src/rvgpu-renderer/rvgpu-renderer -b 800x600@0,0 -p 55667 &
 #disown -a 
